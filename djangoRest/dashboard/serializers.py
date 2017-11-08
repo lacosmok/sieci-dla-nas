8c1a17008323cbd40d01bee5cce5692ce3d49241
lacosmok <lacosmok@gmail.com>
2017-11-08 11:04:51 +0100

    feat: add comment seralzer and model

---
+++ djangoRest/dashboard/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from . import models
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Comment
+        fields = '__all__'
--- djangoRest/dashboard/urls.py
+++ djangoRest/dashboard/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-
+    url(r'^article/(?P<pk>[0-9]+)/comments/$',
+        views.ArticleView.as_view(), name='get-comments'),
 ]
