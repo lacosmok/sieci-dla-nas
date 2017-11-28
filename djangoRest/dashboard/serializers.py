ab90a8160908182745c63edd59120b3741b32333
lacosmok <lacosmok@gmail.com>
2017-11-28 11:40:13 +0100

    feat: added list of networks to backend

--- djangoRest/dashboard/serializers.py
+++ djangoRest/dashboard/serializers.py
@@ -6,3 +6,18 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Comment
         fields = '__all__'
+
+
+class NodeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.Node
+        fields = '__all__'
+
+
+class NetworkSerializer(serializers.ModelSerializer):
+    nodes = NodeSerializer(many=True)
+
+    class Meta:
+        model = models.Network
+        fields = ('name', 'nodes')
--- djangoRest/dashboard/urls.py
+++ djangoRest/dashboard/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^article/(?P<pk>[0-9]+)/comments/$',
         views.ArticleView.as_view(), name='get-comments'),
+
+    url(r'^network/all/$',
+        views.GetNetworks.as_view(), name='get-comments'),
 ]
